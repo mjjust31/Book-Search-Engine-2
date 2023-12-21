20090d64e40213f97b77dc7555c5c92cf0389db2
justmann <mj.justmann@gmail.com>
2023-12-21 13:09:56 -0600

    wrappering-app-in-Apollo-Provider

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,11 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 3006,
     open: true,
     proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
+      '/graphql': {
+        target: 'http://localhost:3006',
         secure: false,
         changeOrigin: true
       }
