5b795a24ebb5d851ac0676ee345219e58776b4b8
justmann <mj.justmann@gmail.com>
2024-01-03 12:04:42 -0600

    redo

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,13 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+const connectionString = process.env.MONGODB_CONNECT_URI || 'mongodb://127.0.0.1:27017/googlebooks';
+
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
 
 module.exports = mongoose.connection;
